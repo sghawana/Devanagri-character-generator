466adfecc9fd260e140435cb53801c7819f44cac
sghawana <59884048+sghawana@users.noreply.github.com>
2024-10-18 03:05:31 +0530

    classes

--- src/discriminator.py
+++ src/discriminator.py
@@ -3,41 +3,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Discriminator_CNN(nn.Module):
-    def __init__(self, activ, device, dtype):
+    def __init__(self, device, dtype):
         super().__init__()
-        self.activ = activ
         self.device = device
         self.dtype = dtype
-            
-        ## out = floor[ (in-kernel)/stride   +   1]
-        ## 32 --> 14 --> 5 --> Linear Layers
-        self.conv1 = nn.Conv2D(in_channels=1, out_channels=4, kernel_size=5, stride=2, device=self.device, dtype=self.dtype)
-        self.conv2 = nn.Conv2D(in_channels=4, out_channels=8, kernel_size=5, stride=2, device=self.device, dtype=self.dtype)
-        
-        self.linear1 = nn.Linear(in_features=8*5*5, out_features=64)
-        self.linear2 = nn.Linear(in_features=64, out_features=16)
-        self.linear3 = nn.Linear(in_features=16, out_features=1) 
 
-    def activation(self, x):
-        if self.activ == 'relu':
-            return F.relu(x)
-        elif self.activ == 'sigmoid':
-            return F.sigmoid(x)
-        elif self.activ == 'tanh':
-            return F.tanh(x)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, stride=2, device=self.device, dtype=self.dtype)
+        self.bn1 = nn.BatchNorm2d(num_features=64, device=self.device, dtype=self.dtype)
+        self.d1 = nn.Dropout2d(p=0.2)
         
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, device=self.device, dtype=self.dtype)
+        self.bn2 = nn.BatchNorm2d(num_features=128, device=self.device, dtype=self.dtype)
+        self.d2 = nn.Dropout2d(p=0.2)
+        
+        self.linear1 = nn.Linear(in_features=128*5*5, out_features=100, device=self.device, dtype=self.dtype)
+        self.bn3 = nn.BatchNorm1d(num_features=100, device=self.device, dtype=self.dtype)
+        self.d3 = nn.Dropout1d(p=0.2)
+        
+        self.linear2 = nn.Linear(in_features=100, out_features=1, device=self.device, dtype=self.dtype)
+
     def forward(self, x):
         x = self.conv1(x)
-        x = self.activation(x)
+        x = self.bn1(x)
+        x = F.tanh(x)
+        x = self.d1(x)
+
         x = self.conv2(x)
-        x = self.activation(x)
-        x = x.reshape(x.shape[0], -1)
-        
+        x = self.bn2(x)
+        x = F.tanh(x)
+        x = self.d2(x)
+
+        x = x.reshape(-1, 128*5*5)
+
         x = self.linear1(x)
-        x = self.activation(x)
+        x = x = F.tanh(x)
+
         x = self.linear2(x)
-        x = self.activation(x)
-        x = self.linear3(x)
         x = F.sigmoid(x)
         return x
-        
--- src/generator.py
+++ src/generator.py
@@ -3,35 +3,53 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Generator_CNN(nn.Module):
-    def __init__(self, activ, device, dtype):
+    def __init__(self, device, dtype):
         super().__init__()
-        self.activ = activ
         self.device = device
         self.dtype = dtype
         
-        ## out = (in-1)*stride + kernel
-        ## 3 --> 7 --> 15 --> 22 --> 28+4
-        self.conv1 = nn.ConvTranspose2d(in_channels=1, out_channels=4, kernel_size=3, stride=2, device=self.device, dtype=self.dtype)
-        self.conv2 = nn.ConvTranspose2d(in_channels=4, out_channels=4, kernel_size=3, stride=2, device=self.device, dtype=self.dtype)
-        self.conv3 = nn.ConvTranspose2d(in_channels=4, out_channels=2, kernel_size=7, stride=1, device=self.device, dtype=self.dtype)
-        self.conv4 = nn.ConvTranspose2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, output_padding=2, device=self.device, dtype=self.dtype)
+        self.project = nn.Linear(in_features=100, out_features=1024*4*4, device=self.device, dtype=self.dtype)
 
-    def activation(self, x):
-        if self.activ == 'relu':
-            return F.relu(x)
-        elif self.activ == 'sigmoid':
-            return F.sigmoid(x)
-        elif self.activ == 'tanh':
-            return F.tanh(x)
+        self.conv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=256, kernel_size=3, stride=2, padding=1, device=self.device, dtype=self.dtype)
+        self.bn1 = nn.BatchNorm2d(num_features=256, device=self.device, dtype=self.dtype)
+        self.d1 = nn.Dropout2d(p=0.2)
         
+        self.conv2 = nn.ConvTranspose2d(in_channels=256, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1, device=self.device, dtype=self.dtype)
+        self.bn2 = nn.BatchNorm2d(num_features=32, device=self.device, dtype=self.dtype)
+        self.d2 = nn.Dropout2d(p=0.2)
+        
+        self.conv3 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, device=self.device, dtype=self.dtype)
+        self.bn3 = nn.BatchNorm2d(num_features=32, device=self.device, dtype=self.dtype)
+        self.d3 = nn.Dropout2d(p=0.2)
+        
+        self.conv4 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=3, stride=1, padding=1, device=self.device, dtype=self.dtype)
+        
+
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.activation(x)
-        x = self.conv2(x)
-        x = self.activation(x)
-        x = self.conv3(x)
-        x = self.activation(x)
+
+        x = self.project(x)
+        x = F.tanh(x)
+        x = x.reshape(-1, 1024, 4, 4)
+        
+        x = self.conv1(x, output_size=(-1, 512, 8, 8))
+        x = self.bn1(x)
+        x = F.tanh(x)
+        x = self.d1(x)
+        
+        x = self.conv2(x, output_size=(-1, 256, 16, 16))
+        x = self.bn2(x)
+        x = F.tanh(x)
+        x = self.d2(x)
+        
+        x = self.conv3(x, output_size=(-1, 128, 32, 32))
+        x = self.bn3(x)
+        x = F.tanh(x)
+        x = self.d3(x)
+        
         x = self.conv4(x)
+        x = F.sigmoid(x)
+
         return x
+
         
         
